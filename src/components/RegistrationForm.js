56c2994656bec52a303cec8a6ac2f09955177206
Michael Boroski <mjboroski@gmail.com>
2018-07-25 23:34:51 +0000

    automatically backed up by learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,4 +1,13 @@
-class RegistrationForm extends component{
+import React from 'react';
 
-  
+export default class RegistrationForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="text" />
+        <input type="password" />
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
 }
